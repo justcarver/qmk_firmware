a2ccd23b5242631a0b9661620ed3a4a01c24660f
Justin Carver <carver@justcarver.com>
2017-10-26 20:36:42 -0400

    Fixed a typo in my Ergodox Infinity layout

--- keyboards/ergodox_infinity/keymaps/carver/keymap.c
+++ keyboards/ergodox_infinity/keymaps/carver/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, 	KC_Q,     KC_W,	    KC_E,     KC_R,	  KC_T,	      MO(SYMB),
         CTL_T(KC_ESC),	KC_A,     KC_S,	    KC_D,     KC_F,	  KC_G,
         KC_LSFT,	KC_Z,	  KC_X,	    KC_C,     KC_V,	  KC_B,	      MO(MDIA),
-        KC_Ralt,	KC_LBRC,  KC_RBRC,  TG(MDIA), TG(SYMB),
+        KC_LALT,	KC_LBRC,  KC_RBRC,  TG(MDIA), TG(SYMB),
 								  S(KC_TAB),  KC_LGUI,
 									      KC_HOME,
 							KC_BSPC,  KC_TAB,     KC_END,
